75bf3ecb8faa77b7c730afd843c85b1d435f45f0
Victor Quiroz Castro <victorhqc@gmail.com>
2019-07-15 20:29:16 +0200

    Simplify a bit

--- gif-service/src/main.rs
+++ gif-service/src/main.rs
@@ -1,4 +1,3 @@
-use futures::future::lazy;
 use std::{thread, time};
 
 use futures::{Future};
@@ -13,32 +12,34 @@ use crate::redis::{save_in_redis};
 
 fn main() {
     loop {
-        tokio::run(lazy(move || {
-            let words = vec![
-                String::from("pug"),
-                String::from("cat"),
-                String::from("llama"),
-            ];
-
-            let word = get_random_word(words);
-            let word = match word {
-                Some(word) => word,
-                None => panic!("No random word!"),
-            };
-
-            println!("Fetching gifs for {}", word);
-            fetch_gifs(word).and_then(|gifs| {
-                println!("Obtained {} new gifs", gifs.len());
-                save_in_redis(gifs);
-                Ok(())
-            }).map_err(|e| {
-                println!("Oh no! {:?}", e);
-            })
-        }));
+        tokio::run(gif_service());
 
         let five_mins = time::Duration::from_secs(60 * 5);
         thread::sleep(five_mins);
     }
+}
+
+fn gif_service() -> impl Future<Item=(), Error=()> {
+    let words = vec![
+        String::from("pug"),
+        String::from("cat"),
+        String::from("llama"),
+    ];
+
+    let word = get_random_word(words);
+    let word = match word {
+        Some(word) => word,
+        None => panic!("No random word!"),
+    };
+
+    println!("Fetching gifs for {}", word);
+    fetch_gifs(word).and_then(|gifs| {
+        println!("Obtained {} new gifs", gifs.len());
+        save_in_redis(gifs);
+        Ok(())
+    }).map_err(|e| {
+        println!("Oh no! {:?}", e);
+    })
 }
 
 fn get_random_word(words: Vec<String>) -> Option<String> {
